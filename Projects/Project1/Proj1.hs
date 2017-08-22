5cb1c09851841715ed41a930ac3ff39720ad2119
EmanMacario <emacario1997@hotmail.com>
2017-08-22 23:14:31 +1000

    Initial search space and working solution for Hint #1 added

--- Projects/Project1/Proj1.hs
+++ Projects/Project1/Proj1.hs
@@ -14,6 +14,9 @@
 
 module Proj1 (initialGuess, nextGuess, GameState) where
 
+import Data.List
+import qualified Data.Set as Set
+
 
 -- Basic Types for a 'Pitch'
 type Note   = Char
@@ -23,36 +26,43 @@ type Octave = Char
 data Pitch = Pitch {
   note   :: Note
 , octave :: Octave
-} deriving (Show)
+} deriving (Show, Eq)
 
 -- 'Chord' Type, containing three distinct instances of a 'Pitch'
-data Chord = Chord {
-     p1   :: Pitch
-,    p2   :: Pitch
-,    p3   :: Pitch
-} deriving (Show)
+data Chord = Chord { 
+  p1  :: Pitch
+, p2  :: Pitch
+, p3  :: Pitch
+} deriving (Show, Eq)
 
 
 -- 'TargetChords' Type, which contains all remaining possible chords,
 -- derived from both previous guesses and previous answers.
-type TargetChords = [Chord]
+type SearchSpace = [[String]]
 
 
--- 'PreviousGuesses' Type is a list of 3-tuples of integers, used for holding
--- answers from previous guesses.
-type PreviousGuesses = [(Int, Int, Int)]
+-- 'PreviousGuesses' Type is a list of chords, used for holding
+-- previous guesses.
+type PreviousGuesses = [Chord]
+
+
+-- 'PreviousAnswers' Type is a list of 3-tuples of integers, used for
+-- holding answers to previous guesses.
+type PreviousAnswers = [(Int, Int, Int)]
 
 
 -- 'GameState' type holds the current state of the game
-data GameState = GameState TargetChords 
-
+data GameState = GameState {
+    targets :: SearchSpace
+}
 
 -- Testing syntax
 targetChords =  [
-                    Chord {
-                        p1 = Pitch {note = 'A', octave = '1'}
-                    ,   p2 = Pitch {note = 'B', octave = '3'}
-                    ,   p3 = Pitch {note = 'C', octave = '2'}
+                    Chord 
+                    { 
+                      p1 = Pitch {note = 'A', octave = '1'}
+                    , p2 = Pitch {note = 'B', octave = '3'}
+                    , p3 = Pitch {note = 'C', octave = '2'}
                     },
 
                     Chord {
@@ -63,22 +73,52 @@ targetChords =  [
                 ]
 
 
+-- Valid notes and octaves.
+noteChars = "ABCDEFG"
+octaveChars = "123"
+
+-- Enumerates all possible pitches.
+allPitches = [ [p, o] | p <- noteChars, o <- octaveChars ]
+
+-- Enumerates all possible target chords prior to game start, ensuring
+-- that there are no duplicates.
+searchSpace = nub $ map sort allChords
+    where allChords =                                                            -- Note:
+            [ [p1, p2, p3] | p1 <- allPitches,                                   -- Could just change [p1,p2,p3] if you wanted
+                             p2 <- filter (\p -> p /= p1) allPitches,            -- to use another type instead of list to store
+                             p3 <- filter (\p -> p /= p1 && p /= p2) allPitches  -- target chords.
+            ]
+
+
+
+-- Now, initialise the 'GameState' before the game begins.
+gamestate = GameState {
+    targets = tail searchSpace
+}
+
+
 
 -- Takes no input arguments, and returns a 2-tuple, including 
 -- an initial guess chord and the intial game state.
 initialGuess :: ([String], GameState)
-initialGuess = (["A1", "B2", "C3"], GameState targetChords)
+initialGuess = (head searchSpace, gamestate)
 
 
 -- Takes as input a pair of the previous guess and game state, and the feedback
 -- to this guess as a triple of correct pitches, notes, and octaves, and returns
 -- a pair of the next guess and game state.
 nextGuess :: ([String], GameState) -> (Int, Int, Int) -> ([String], GameState)
-nextGuess ((c1:c2:c3:[]), game) (p,n,o) = (["A3", "B2", "G2"], 
-                                                        GameState targetChords)
+nextGuess ((c1:c2:c3:[]), gamestate) (p,n,o) = (head $ targets gamestate, 
+                                                updateGameState gamestate)
 
 
 
+
+updateGameState :: GameState -> GameState
+updateGameState gamestate = newgamestate
+    where newgamestate = GameState {
+                            targets = tail $ targets gamestate
+                         }
 
 
 
@@ -94,7 +134,6 @@ nextGuess ((c1:c2:c3:[]), game) (p,n,o) = (["A3", "B2", "G2"],
 
 GAME IDEAS:
 
-We end our guess when it is (3,0,0)
 
 
 -}
