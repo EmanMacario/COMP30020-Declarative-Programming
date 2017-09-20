766422f4b82adc0a27535b29b07f6990ba8f0586
EmanMacario <emacario1997@hotmail.com>
2017-09-20 10:26:47 +1000

    Added more of own solutions to WS06

--- Workshops/WS06/world.pl
+++ Workshops/WS06/world.pl
@@ -4,24 +4,41 @@
 :- ensure_loaded(rivers).
 
 
+% Question 1 - Write a query to find what borders Australia.
+% ?- borders(australia, X).
+
+
+% Question 2 - Query to find what shares a border with both France and Spain
+% ?- borders(X, france), borders(X, spain).
+
+
+% Question 3 - Give a query to find what countries share a border with both
+% France and Spain. Use the country/2 query.
+% ?- borders(X, france), borders(X, spain), country(X).
+
+
 % Question 4
 country(X) :- country(X, _, _, _, _, _, _, _).
 
+
 % Question 5
-larger(X, Y) :- country(X, _, _, _, AreaX, _, _, _),
-                country(Y, _, _, _, AreaY, _, _, _),
-                AreaX > AreaY.
+larger(C1, C2) :- country(C1, _, _, _, A1, _, _, _),
+                  country(C2, _, _, _, A2, _, _, _),
+                  A1 > A2.
 
 % Question 6
-river_country(River, Country) :- river(River, [X | Xs]),
+river_country(River, Country) :- river(River, CountryList),
                                  country(Country),
-                                 member(Country, [X | Xs]). 
+                                 member(Country, CountryList). 
 
 
 country_region(Country, Region) :- country(Country, Region, _, _, _, _, _, _).
 
 
-/* Query:
-country_region(C1, R1), country_region(C2, R2), R1 \= R2, 
-river_country(River, C1), river_country(River, C2).
-*/
+% Give a query to find a river that flows between countries in different
+% regions.
+% ?- country_region(C1, R1), 
+%    country_region(C2, R2), 
+%    R1 \= R2, 
+%    river_country(River, C1), 
+%    river_country(River, C2).
