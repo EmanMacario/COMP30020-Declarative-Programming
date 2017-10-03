9ec8764555b0d0281624437a02bed0e2ab520cd5
EmanMacario <emacario1997@hotmail.com>
2017-10-03 16:32:46 +1100

    Added WS08 final solutions

--- Workshops/WS08/workshop08.pl
+++ Workshops/WS08/workshop08.pl
@@ -1,35 +1,54 @@
 % COMP30020 Week 9 - Workshop 8
 
 % Question 1
-
 sumlist(L, Sum) :-
-    sumlist_acc(L, Sum, 0).
+    sumlist_acc(L, 0, Sum).
 
 sumlist_acc([], Acc, Acc).
-sumlist_acc([N|Ns], Sum, Acc) :- 
+sumlist_acc([N|Ns], Acc, Sum) :- 
     NewAcc is Acc + N,
-    sumlist_acc(Ns, Sum, NewAcc).
+    sumlist_acc(Ns, NewAcc, Sum).
 
 
-% Question 2
+% Predicate tree/1 checks if a BST is 'proper'.
 tree(empty).
 tree(node(Left, _, Right)) :-
     tree(Left),
     tree(Right).
 
+
+% Question 2
 tree_list(empty, []).
-tree_list(node(L, V, R), List) :-
-    tree_list(L, LeftList),
-    tree_list(R, RightList),
-    append(LeftList, [V|RightList], List).
+tree_list(node(Left, N, Right), List) :-
+    tree_list(Left, L),
+    tree_list(Right, R),
+    append(L, [N|R], List).
 
 
-% Better Implementation.
-tree_list1(empty, Acc, Acc).
-tree_list1(node(L, V, R), AccIn, AccOut) :-
-    tree_list1(R, AccIn, Acc1),
-    Acc2 = [V|Acc1],
-    tree_list1(L, Acc2, AccOut).
+
+% Question 3 - Better TRO Implementation.
+tree_list(empty, Acc, Acc).
+tree_list(node(L, V, R), AccIn, AccOut) :-
+    tree_list(R, AccIn, AccOutRight),
+    AccInLeft = [V|AccOutRight],
+    tree_list(L, AccInLeft, AccOut).
 
 
 % T = node(node(empty,1,empty), 2, node(empty,3,empty)).
+
+
+
+% Question 4 - list_tree/2 recursively constructs a BST from a list
+% starting from the middle element as the root node.
+list_tree([], empty).
+list_tree([E|List], node(Left,Elt,Right)) :-
+    length(List, Len),
+    Len2 is Len // 2,
+    length(Front, Len2),
+    append(Front, [Elt|Back], [E|List]),
+    list_tree(Front, Left),
+    list_tree(Back, Right).
+
+
+
+
