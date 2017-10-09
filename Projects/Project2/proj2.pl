158b03f3ba9fc5d233eacdc9a855f12954c4221a
EmanMacario <emacario1997@hotmail.com>
2017-10-09 22:57:37 +1100

    Started 3x3 puzzle solver. Added nicely formatted 3x3 test cases

--- Projects/Project2/proj2.pl
+++ Projects/Project2/proj2.pl
@@ -25,26 +25,65 @@ between/3
 
 puzzle_solution(Puzzle) :-
     length(Puzzle, N),
-    (N #= 2 ; N #= 3; N #= 4),
     maplist(same_length(Puzzle), Puzzle),
+    ( N #= 3 ->
+        puzzle_solution2(Puzzle)
+    ;
+        puzzle_solution3(Puzzle)
+    ).
+    
+
+
+% Predicate to solve 2x2 puzzles
+puzzle_solution2(Puzzle) :-
     Puzzle = [[_,W,X],
               [Y,A,B],
               [Z,C,A]],
-    Puzzle = [R1, [R2H|R2T], [R3H|R3T]],
+    Puzzle = [_, [_|R2T], [_|R3T]],
 
     % Check that all values are in the domain.
     append([R2T, R3T], Vs), Vs ins 1..9,
     
-
+    % Check each row and column is distinct.
     maplist(all_distinct, [R2T, R3T]),
     transpose([R2T, R3T], Columns),
     maplist(all_distinct, Columns), 
     
-
+    % Set arithmetic constraints.
     (Y #= A + B ; Y #= A * B),
     (Z #= C + A ; Z #= C * A),
     (W #= C + A ; W #= C * A),
     (X #= B + A ; X #= B * A),
+
+    % Write out the puzzle solution.
+    maplist(writeln, Puzzle).
+
+
+% Predicate to solve 3x3 puzzles.
+puzzle_solution3(Puzzle) :-
+    Puzzle = [[_, A, B, C],
+              [D, X, G, H],
+              [E, I, X, J],
+              [F, K, L, X]],
+    Puzzle = [_, [_|R2T], [_|R3T], [_|R4T]],
+
+    % Check that all values are in the domain.
+    append([R2T, R3T, R4T], Vs), Vs ins 1..9,
+
+    % Check each row and column is distinct.
+    maplist(all_distinct, [R2T, R3T, R4T]),
+    transpose([R2T, R3T, R4T], Columns),
+    maplist(all_distinct, Columns),
+
+    % Set arithmetic constraints.
+    (D #= X + G + H ; D #= X * G * H),
+    (E #= I + X + J ; E #= I * X * J),
+    (F #= K + L + X ; F #= K * L * X),
+    (A #= X + I + K ; A #= X * I * K),
+    (B #= G + X + L ; B #= G * X * L),
+    (C #= H + J + X ; C #= H * J * X),
+
+    % Write out the puzzle solution.
     maplist(writeln, Puzzle).
 
 
@@ -134,5 +173,51 @@ puzzle_solution([[0,15,32],[32,8,4],[56,7,8]]).
 puzzle_solution([[0,14,27],[27,_,_],[45,_,_]]).
 puzzle_solution([[0,14,27],[27,9,3],[45,5,9]]).
 
+*/
+
+
+/* Test cases (3x3)
+puzzle_solution([[0,11,54,45],[16,_,_,9],[35,_,_,_],[27,_,_,_]]).
+puzzle_solution([[0, 11,54,45],
+                 [16, 1, 6, 9],
+                 [35, 7, 1, 5],
+                 [27, 3, 9, 1]]).
+
+
+puzzle_solution([[0,252,35,168],[11,_,_,_],[19,_,_,_],[21,_,_,_]]). 
+puzzle_solution([[0,252,35,168],
+                 [11, 7, 1, 3],
+                 [19, 4, 7, 8],
+                 [21, 9, 5, 7]]).
+
+puzzle_solution([[0,14,18,48],[20,_,_,_],[9,_,_,_],[126,_,_,_]]). 
+puzzle_solution([[0, 14,18,48],
+                 [20, 3, 9, 8],
+                 [9,  4, 3, 2],
+                 [126,7, 6, 3]]).
+
+puzzle_solution([[0,72,336,23],[22,_,_,_],[17,_,_,_],[18,_,_,_]]). 
+puzzle_solution([[0, 72,336,23],
+                 [22, 6, 7, 9],
+                 [17, 3, 6, 8],
+                 [18, 4, 8, 6]]).
+
+puzzle_solution([[0,14,140,40],[56,_,_,_],[140,_,_,_],[60,_,_,_]]). 
+puzzle_solution([[0, 14,140,40],
+                 [56, 4, 7, 2],
+                 [140,7, 4, 5],
+                 [60, 3, 5, 4]]).
+
+puzzle_solution([[0,14,16,18],[189,_,_,_],[17,_,_,_],[60,_,_,_]]). 
+puzzle_solution([[0,14,16,18],[189,3,9,7],[17,6,3,8],[60,5,4,3]]).
+
+puzzle_solution([[0,42,16,9],[48,_,_,_],[24,_,_,_],[105,_,_,_]]). 
+puzzle_solution([[0,42,16,9],[48,3,8,2],[24,2,3,4],[105,7,5,3]]).
+
+puzzle_solution([[0,14,189,20],[14,_,_,_],[18,_,_,_],[63,_,_,_]]). 
+puzzle_solution([[0,14,189,20],[14,7,3,4],[18,2,7,9],[63,1,9,7]]).
+
+puzzle_solution([[0,11,9,162],[14,_,_,_],[18,_,_,_],[84,_,_,_]]). 
+puzzle_solution([[0,11,9,162],[14,3,2,9],[18,1,3,6],[84,7,4,3]]).
 */
 
